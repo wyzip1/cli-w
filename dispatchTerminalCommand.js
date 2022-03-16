9d7e52f1a500994741c4f1a448dcf248de365e60
wyzip <2362086452@qq.com>
2022-03-16 11:58:17 +0800

    update: command

--- dispatchTerminalCommand.js
+++ dispatchTerminalCommand.js
@@ -1,19 +1,29 @@
 #!/usr/bin/env node
 
+const packageJson = require('./package.json');
 const createFolder = require("./createFolder");
 const program = require("commander");
 
 program
-  .command("crf")
-  .allowUnknownOption()
-  .option("-r, --create-react <folderName>")
-  .option("-v, --create-vue <folderName>")
-  .option("-j, --create-js <folderName>")
+  .command("react [name]")
   .action((args) => {
-    createFolder(args);
+    createFolder({ createReact: args });
+  });
+
+program
+  .command("vue [name]")
+  .action((args) => {
+    createFolder({ createVue: args });
+  });
+
+program
+  .command("js [name]")
+  .allowUnknownOption()
+  .action((args) => {
+    createFolder({ createJs: args });
   });
 
 // commands
-program.version(`w-cli v${process.version}`);
+program.version(`w-cli v${packageJson.version}`);
 
 program.parse(process.argv);
