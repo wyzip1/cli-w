97c9dd68bd901db12392f2c1ac2dbb4de4e41698
wyzip <2362086452@qq.com>
2022-03-16 11:25:25 +0800

    update

--- utils/formatName.js
+++ utils/formatName.js
@@ -1,7 +1,10 @@
 function formatName (name) {
-  return name.split('/').at(-1).split('-').map(item => {
+  const list = name.split('/')
+  const fname = list.at(-1).split('-').map(item => {
     return item[0].toUpperCase() + item.slice(1)
   }).join('');
+  list[list.length - 1] = fname;
+  return list.join('/')
 }
 
 module.exports = formatName
