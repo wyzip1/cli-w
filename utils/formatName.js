8a64a1a166f6962aea84072621b11982fe269193
wyzip <2362086452@qq.com>
2022-03-16 10:58:17 +0800

    update

--- utils/formatName.js
+++ utils/formatName.js
@@ -1,5 +1,5 @@
 function formatName (name) {
-  return name.split('-').map(item => {
+  return name.split('/').at(-1).split('-').map(item => {
     return item[0].toUpperCase() + item.slice(1)
   }).join('');
 }
