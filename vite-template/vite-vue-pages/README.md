c927fbc6f91c85aaf6f623da4bf4972ae554957c
wyzip <2362086452@qq.com>
2022-07-08 14:48:00 +0800

    模版完善

---
+++ vite-template/vite-vue-pages/README.md
@@ -0,0 +1,5 @@
+# npm run fix-build:css
+
+修复vite-ftl-plugin打包丢失组件样式问题
+
+!! 使用npm install , cnpm 安装依赖会导致文件目录结构差异，vite-plugin-style-import插件会因为找不到style报错
--- vite-template/vite-vue-pages/src/main.js
+++ vite-template/vite-vue-pages/src/main.js
@@ -1,6 +1,5 @@
 import App from './App.vue'
 import { createApp } from 'vue'
-import 'styles/_index.less'
-import 'styles/_reset.less'
+import 'styles/_base.scss'
 
 createApp(App).mount('#root')
